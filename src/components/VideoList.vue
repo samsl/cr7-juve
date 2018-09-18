27e5f09cd94550859925d6ad931638550bb47381
samsun <samsunsl@163.com>
2018-09-18 22:45:19 +0800

    Remove preview logic for mobile

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="video-list">        
 	<div class="video-container mobile mobile-tight" v-for="video in videos">               
-        <div class="video"v-on:mouseover="playVideo(true, $event)" v-on:mouseout="playVideo(false, $event)">
+        <div class="video">
              <router-link :to="{name:'video', params:{id: video.title}}">
 	    	<video muted>
 	    		<source :src="'/static/video/' + video.title" type="video/mp4"/> Your browser does not support the video tag.
